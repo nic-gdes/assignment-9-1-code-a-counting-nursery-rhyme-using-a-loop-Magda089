e10979b72dac0c96f93884fbcbbd422cd494c93e
Magdalena Idzikowska <midzikowska@students.nic.edu>
2023-10-19 14:11:06 -0700

    small changes

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ while (count > 0) {
     console.log(count + " " + word + " went out one day,");
     console.log("Over the hill and far away,");
     console.log("Mother duck said, 'Quack, quack, quack, '");
-    console.log("but only " + (count - 1) + " little" + word + "came back.");
+    console.log("but only " + (count - 1) + " little " + word + " came back.");
     count = count - 1;
 
     if (count > 1) {
         console.log();
-    } else {
-        console.log("No more" + word + "went out one day.");
+    } else   {
+        console.log("No more " + word + " went out one day.");
+        // break;
     }
     count --;
 }
