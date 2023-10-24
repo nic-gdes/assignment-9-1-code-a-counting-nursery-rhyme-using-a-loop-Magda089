19852b72c09a786d4b45d1e89aba101dc8333c0e
Magdalena Idzikowska <midzikowska@students.nic.edu>
2023-10-24 15:37:54 -0700

    dom manipulation

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 var word = "ducks";
 var count = 5;
 
-while (count > 0) {
-    console.log(count + " " + word + " went out one day,");
-    console.log("Over the hill and far away,");
-    console.log("Mother duck said, 'Quack, quack, quack, '");
-    console.log("but only " + (count - 1) + " little " + word + " came back.");
-    count = count - 1;
+const list = document.querySelector('#list');
 
-    if (count > 1) {
-        console.log();
-    } else   {
-        console.log("No more " + word + " went out one day.");
-        // break;
-    }
-    count --;
+for(var count = 5; count >= 0; count--) {
+   let textString = '${ count } little ducks went out one day, Over the hill and far away, Mother duck said, Quack, quack, quack, but only ${ count - 1 } little ducks came back.'
+
+   const verse = document.createElement ('p');
+   verse.textContent = textString;
+
+   list.appendChild(verse);
+   
+    // console.log(count + " " + word + " went out one day,");
+    // console.log("Over the hill and far away,");
+    // console.log("Mother duck said, 'Quack, quack, quack, '");
+    // console.log("but only " + (count - 1) + " little " + word + " came back.");
+
+    // if (count > 1) {
+    //     console.log();
+    // } else {
+    //     console.log("No more " + word + " went out one day.");
+    // }
+
+
+
+ 
+        // consol.log(textString);
+        // const verse = document.createElement('p');
+        // verse.textContent = textString; '${ 1 } bottltes of beer on th wall, ${ 1 } bottles of beer. Take one down pass ir around ${ 1 - 1 } botttles of beer on the wall.
+
+        // list.appendChild(verse);    
 }
